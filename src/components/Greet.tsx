4ee6d7531e48a883a2c91bd4c823a43d1afa63c3
SolomonKitonyi <kitonyisolomon@gmail.com>
2021-09-09 09:13:51 +0300

    initial commit

---
+++ src/components/Greet.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+type GreetProps = {
+    name: string
+};
+
+export const Greet = (props: GreetProps) => {
+    return (
+        <h2>Hello {props.name}</h2>
+    );
+}
