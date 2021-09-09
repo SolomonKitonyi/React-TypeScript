480e84363c1226c72b6489dbea73e2a6a7968af9
SolomonKitonyi <kitonyisolomon@gmail.com>
2021-09-09 10:23:34 +0300

    add Personlist component

---
+++ src/components/PersonList.tsx
@@ -0,0 +1,17 @@
+import React from "react";
+
+type PersonListProps = {
+    names: {
+        first: string,
+        last: string
+    }[]
+}
+export const PersonList = (props: PersonListProps) => {
+    return(
+        <div>
+            {props.names.map(name =>(
+                <h2 key={name.first}>{name.first} {name.last}</h2>
+            ))}
+        </div>
+    );
+}
