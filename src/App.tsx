00969e24fce33f05f0424584980ddf0bb4358e0c
SolomonKitonyi <kitonyisolomon@gmail.com>
2021-09-09 10:22:46 +0300

    work with differnt types

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,34 @@
 import React from 'react';
 import './App.css';
 import { Greet } from './components/Greet';
+import { Person } from './components/Person';
+import { PersonList } from './components/PersonList';
 
 const  App = () => {
+  const personName = {
+    first: "Solomon",
+    last: "Kitonyi"
+  };
+  const personList = [
+    {
+      first: "Solomon",
+      last: "Kitonyi"
+    },
+    {
+      first: "Joyce",
+      last: "Munyao"
+    },
+    {
+      first: "John",
+      last: "Wayne"
+    },
+
+  ]
   return (
     <div className="App">
       <Greet name="Solomon" messageCount={10} isLoggedIn={false}/> 
+      <Person name={personName} />
+      <PersonList names = {personList} />
     </div>
   );
 }
