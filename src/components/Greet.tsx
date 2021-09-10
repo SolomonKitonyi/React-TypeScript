17806985b325334e41fe9d85c01317d27557ac0d
SolomonKitonyi <kitonyisolomon@gmail.com>
2021-09-10 10:30:32 +0300

    add optional props

--- src/components/Greet.tsx
+++ src/components/Greet.tsx
@@ -2,14 +2,18 @@ import React from 'react';
 
 type GreetProps = {
     name: string,
-    messageCount: number,
+    messageCount?: number,
     isLoggedIn: boolean
 };
 
 export const Greet = (props: GreetProps) => {
+    let message;
+    if(props.messageCount === undefined){
+        message = 0
+    }
     return (
         <h2>
-          {props.isLoggedIn ? 'Hello {props.name} you have {props.messageCount} unread messages': 'Welcome Guest' }
+          {props.isLoggedIn ? `Hello ${props.name} you have ${props.messageCount || message } unread messages`: 'Welcome Guest' }
         </h2>     
     );
 }
