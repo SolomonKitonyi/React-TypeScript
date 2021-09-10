dc56efbbf808ff6640a2600eb4e3da2a50a5c89f
SolomonKitonyi <kitonyisolomon@gmail.com>
2021-09-10 10:29:05 +0300

    add changes

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,14 @@
 import React from 'react';
 import './App.css';
+import { Button } from './components/Button';
+import { Container } from './components/Container';
 import { Greet } from './components/Greet';
+import { Header } from './components/Header';
+import { Input } from './components/Input';
+import { Oscar } from './components/Oscar';
 import { Person } from './components/Person';
 import { PersonList } from './components/PersonList';
+import { Status } from './components/Status';
 
 const  App = () => {
   const personName = {
@@ -26,9 +32,17 @@ const  App = () => {
   ]
   return (
     <div className="App">
-      <Greet name="Solomon" messageCount={10} isLoggedIn={false}/> 
+      <Greet name="Solomon" isLoggedIn={true}/> 
       <Person name={personName} />
       <PersonList names = {personList} />
+      <Status status="loading" />
+      <Header>Placeholder Text</Header>
+      <Oscar>
+        <Header >Oscar goes to the United States</Header>
+      </Oscar>
+      <Button handleClick={(event,id) => console.log("Button clicked",event,id)}></Button>
+      <Input value='' handleChange={event => console.log(event)} />
+      <Container styles={{border: '1px solid black', padding:"1rem"}} />
     </div>
   );
 }
