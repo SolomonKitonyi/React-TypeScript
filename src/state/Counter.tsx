ce525855b4fcea5fcac007560ee1fb2469a99540
SolomonKitonyi <kitonyisolomon@gmail.com>
2021-09-17 16:15:24 +0300

    add strict types

--- src/state/Counter.tsx
+++ src/state/Counter.tsx
@@ -6,17 +6,23 @@ const initialState = {count: 0};
 type CounterState = {
     count: number
 };
-type CounterAction = {
-    type: string,
+type UpdateAction = {
+    type: 'increment' | 'decrement',
     payload: number
 }
+type ResetAction = {
+    type: 'reset'
+}
+type CounterAction = UpdateAction | ResetAction
 
 function reducer(state: CounterState,action: CounterAction){
     switch(action.type){
         case 'increment':
-            return {count: state.count + action.payload}
+            return {count: state.count + action.payload }
         case 'decrement':
             return {count: state.count - action.payload}
+        case 'reset':
+            return initialState
         default:
             return state
     }
@@ -33,6 +39,7 @@ export const Counter = () => {
          <button onClick={() => dispatch({type: 'decrement',payload: 10})}>
              Decrement 10
          </button>
+         <button onClick={() => dispatch({type: 'reset'})}></button>
         </>
     ); 
 }
